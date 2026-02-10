d8c09467ed25fe3d0805a40e8ce4401ecf52bb27
kairosia <kang970917@gmail.com>
2026-02-10 14:38:00 +0900

    [D2] Title: [파이썬 S/W 문제해결 기본] 4일차 - 종이붙이기, Time: 60 ms, Memory: 51,072 KB -BaekjoonHub

---
+++ SWEA/D2/4869. ［파이썬 S／W 문제해결 기본］ 4일차 － 종이붙이기/［파이썬 S／W 문제해결 기본］ 4일차 － 종이붙이기.py
@@ -0,0 +1,12 @@
+T = int(input())
+ 
+for tc in range(1, T+1):
+    N = int(input())
+     
+    paper = [0] * (N//10)
+    paper[0] = 1
+    paper[1] = 3
+    for i in range(2, N//10):
+        paper[i] = paper[i-1] + paper[i-2]*2
+     
+    print(f'#{tc} {paper[-1]}')
