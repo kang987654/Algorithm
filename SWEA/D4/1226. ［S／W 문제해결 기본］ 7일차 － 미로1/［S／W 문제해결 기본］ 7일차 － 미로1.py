c02207814ae583124dccfea10a7dcfe6bb92c7f5
kairosia <kang970917@gmail.com>
2026-02-13 15:56:43 +0900

    [D4] Title: [S/W 문제해결 기본] 7일차 - 미로1, Time: 79 ms, Memory: 59,392 KB -BaekjoonHub

--- SWEA/D4/1226. ［S／W 문제해결 기본］ 7일차 － 미로1/［S／W 문제해결 기본］ 7일차 － 미로1.py
+++ SWEA/D4/1226. ［S／W 문제해결 기본］ 7일차 － 미로1/［S／W 문제해결 기본］ 7일차 － 미로1.py
@@ -1,3 +1,39 @@
+def bfs():
+    while player:
+        ni, nj = player.pop(0)
+
+        if miro[ni][nj] == '3':
+            break
+
+        for di, dj in move:
+            if 0 <= ni + di < N and 0 <= nj + dj < N and \
+               miro[ni+di][nj+dj] != '1' and not visited[ni+di][nj+dj]:
+                visited[ni+di][nj+dj] = True
+                player.append((ni+di, nj+dj))
+
+
+def dfs_wh():
+    while player:
+        ni, nj = player.pop()
+
+        if miro[ni][nj] == '3':
+            break
+
+        for di, dj in move:
+            if 0 <= ni + di < N and 0 <= nj + dj < N and \
+               miro[ni+di][nj+dj] != '1' and not visited[ni+di][nj+dj]:
+                visited[ni+di][nj+dj] = True
+                player.append((ni+di, nj+dj))
+
+
+def dfs_re(ni, nj):
+    for di, dj in move:
+        if 0 <= ni + di < N and 0 <= nj + dj < N and \
+           miro[ni + di][nj + dj] != '1' and not visited[ni + di][nj + dj]:
+            visited[ni + di][nj + dj] = True
+            dfs_re(ni + di, nj + dj)
+
+
 for _ in range(10):
     tc = input()
     N = 16
@@ -17,16 +53,9 @@ for _ in range(10):
 
     visited[si][sj] = True
     player = [(si, sj)]
-    while player:
-        ni, nj = player.pop(0)
 
-        if miro[ni][nj] == '3':
-            break
-
-        for di, dj in move:
-            if 0 <= ni + di < N and 0 <= nj + dj < N and \
-               miro[ni+di][nj+dj] != '1' and not visited[ni+di][nj+dj]:
-                visited[ni+di][nj+dj] = True
-                player.append((ni+di, nj+dj))
+    bfs()
+    # dfs_wh()
+    # dfs_re(*player[0])
 
     print(f'#{tc} {int(visited[ei][ej])}')
