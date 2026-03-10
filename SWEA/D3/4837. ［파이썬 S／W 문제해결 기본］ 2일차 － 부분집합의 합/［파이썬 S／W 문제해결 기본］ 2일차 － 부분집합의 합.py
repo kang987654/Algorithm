d4472269ddc0cefb430022cf25d1c7fe524af1a8
kairosia <kang970917@gmail.com>
2026-03-10 15:12:43 +0900

    [D3] Title: [파이썬 S/W 문제해결 기본] 2일차 - 부분집합의 합, Time: 124 ms, Memory: 63,232 KB -BaekjoonHub

--- SWEA/D3/4837. ［파이썬 S／W 문제해결 기본］ 2일차 － 부분집합의 합/［파이썬 S／W 문제해결 기본］ 2일차 － 부분집합의 합.py
+++ SWEA/D3/4837. ［파이썬 S／W 문제해결 기본］ 2일차 － 부분집합의 합/［파이썬 S／W 문제해결 기본］ 2일차 － 부분집합의 합.py
@@ -1,20 +1,26 @@
 T = int(input())
 
+
+def sub_sum(n, idx, _sum):
+    global cnt
+
+    if n == N:
+        if _sum == K:
+            cnt += 1
+        return
+
+    if _sum >= K:
+        return
+
+    for i in range(idx, 12):
+        sub_sum(n+1, i+1, _sum+A[i])
+
+
 for tc in range(1, T+1):
     N, K = map(int, input().split())
 
-    # 1~12
-    origin = list(range(1, 13))
-    # 부분 집합의 합을 저장할 리스트
-    sub_sum = []
-    for i in range(1 << 12):
-        sub = []    # 부분 집합
-        cnt = 0     # 부분 집합의 원소 수
-        for j in range(12):
-            if i & (1 << j):
-                sub.append(origin[j])
-                cnt += 1
-        if cnt == N:
-            sub_sum.append(sum(sub))
+    A = list(range(1, 13))
+    cnt = 0
+    sub_sum(0, 0, 0)
 
-    print(f'#{tc} {sub_sum.count(K)}')
+    print(f'#{tc} {cnt}')
