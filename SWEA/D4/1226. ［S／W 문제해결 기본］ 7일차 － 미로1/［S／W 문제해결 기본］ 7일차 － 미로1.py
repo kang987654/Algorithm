e0009fdddd893231330e926f433eb07544ab0348
kairosia <kang970917@gmail.com>
2026-04-08 14:53:50 +0900

    [D4] Title: [S/W 문제해결 기본] 7일차 - 미로1, Time: 79 ms, Memory: 59,392 KB -BaekjoonHub

--- SWEA/D4/1226. ［S／W 문제해결 기본］ 7일차 － 미로1/［S／W 문제해결 기본］ 7일차 － 미로1.py
+++ SWEA/D4/1226. ［S／W 문제해결 기본］ 7일차 － 미로1/［S／W 문제해결 기본］ 7일차 － 미로1.py
@@ -1,61 +1,36 @@
-def bfs():
-    while player:
-        ni, nj = player.pop(0)
-
-        if miro[ni][nj] == '3':
-            break
-
-        for di, dj in move:
-            if 0 <= ni + di < N and 0 <= nj + dj < N and \
-               miro[ni+di][nj+dj] != '1' and not visited[ni+di][nj+dj]:
-                visited[ni+di][nj+dj] = True
-                player.append((ni+di, nj+dj))
+dr = [-1, +1, +0, +0]
+dc = [+0, +0, -1, +1]
 
 
-def dfs_wh():
-    while player:
-        ni, nj = player.pop()
+def able_miro(s, e):
+    next_way = [(s[0], s[1])]
+    miro[s[0]][s[1]] = 1
 
-        if miro[ni][nj] == '3':
-            break
+    while next_way:
+        r, c = next_way.pop()
 
-        for di, dj in move:
-            if 0 <= ni + di < N and 0 <= nj + dj < N and \
-               miro[ni+di][nj+dj] != '1' and not visited[ni+di][nj+dj]:
-                visited[ni+di][nj+dj] = True
-                player.append((ni+di, nj+dj))
-
-
-def dfs_re(ni, nj):
-    for di, dj in move:
-        if 0 <= ni + di < N and 0 <= nj + dj < N and \
-           miro[ni + di][nj + dj] != '1' and not visited[ni + di][nj + dj]:
-            visited[ni + di][nj + dj] = True
-            dfs_re(ni + di, nj + dj)
+        if (r, c) == e:
+            return 1
+        
+        for di, dj in zip(dr, dc):
+            next_r, next_c = r + di, c + dj
+            if 0 <= next_r < N and 0 <= next_c < N and miro[next_r][next_c] != 1:
+                miro[next_r][next_c] = 1
+                next_way.append((next_r, next_c))
+    
+    return 0
 
 
 for _ in range(10):
-    tc = input()
+    tc = int(input())
     N = 16
-    miro = [list(input()) for _ in range(N)]
-
-    si, sj = None, None
-    ei, ej = None, None
+    miro = [list(map(int, input())) for _ in range(N)]
+    
     for i in range(N):
         for j in range(N):
-            if miro[i][j] == '2':
-                si, sj = i, j
-            if miro[i][j] == '3':
-                ei, ej = i, j
+            if miro[i][j] == 2:
+                start = (i, j)
+            if miro[i][j] == 3:
+                end = (i, j)
 
-    move = [(-1, 0), (1, 0), (0, -1), (0, 1)]
-    visited = [[False] * N for _ in range(N)]
-
-    visited[si][sj] = True
-    player = [(si, sj)]
-
-    bfs()
-    # dfs_wh()
-    # dfs_re(*player[0])
-
-    print(f'#{tc} {int(visited[ei][ej])}')
+    print(f'#{tc} {able_miro(start, end)}' )
