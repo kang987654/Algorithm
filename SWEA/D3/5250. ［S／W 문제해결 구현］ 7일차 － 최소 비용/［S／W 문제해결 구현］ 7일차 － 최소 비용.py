61784beba12c6118d0601f86233c48d6b7faa6c0
kairosia <kang970917@gmail.com>
2026-04-13 00:43:09 +0900

    [D3] Title: [S/W 문제해결 구현] 7일차 - 최소 비용, Time: 311 ms, Memory: 68,352 KB -BaekjoonHub

--- SWEA/D3/5250. ［S／W 문제해결 구현］ 7일차 － 최소 비용/［S／W 문제해결 구현］ 7일차 － 최소 비용.py
+++ SWEA/D3/5250. ［S／W 문제해결 구현］ 7일차 － 최소 비용/［S／W 문제해결 구현］ 7일차 － 최소 비용.py
@@ -3,39 +3,33 @@ import heapq
 dr = [-1, +1, +0, +0]
 dc = [+0, +0, -1, +1]
 
-def dijkstra():
-    fuel = [[float('inf')] * N for _ in range(N)]
-    fuel[0][0] = 0
-
-    hq = [(0, 0, 0)]
-
-    while hq:
-        now_fuel, r, c = heapq.heappop(hq)
-
-        if fuel[r][c] < now_fuel:
-            continue
-
-        for di, dj in zip(dr, dc):
-            next_r, next_c = r + di, c + dj
-            if 0 <= next_r < N and 0 <= next_c < N:
-                if H[r][c] < H[next_r][next_c]:
-                    next_fuel = now_fuel + 1 + H[next_r][next_c] - H[r][c]
-                else:
-                    next_fuel = now_fuel + 1
-
-                if next_fuel < fuel[next_r][next_c]:
-                    fuel[next_r][next_c] = next_fuel
-                    heapq.heappush(hq, (next_fuel, next_r, next_c))
-
-    return fuel[N-1][N-1]
-
-
 T = int(input())
 
 for tc in range(1, T+1):
     N = int(input())
     H = [list(map(int, input().split())) for _ in range(N)]
 
-    result = dijkstra()
+    fuels = [[float('inf')] * N for _ in range(N)]
+    fuels[0][0] = 0
+    stack = [(0, 0, 0)]
+    while stack:
+        f, r, c = heapq.heappop(stack)
 
-    print(f'#{tc} {result}')
+        if f > fuels[r][c]:
+            continue
+
+        fuels[r][c] = f
+
+        for di, dj in zip(dr, dc):
+            next_r, next_c = r + di, c + dj
+            if 0 <= next_r < N and 0 <= next_c < N:
+                if H[r][c] < H[next_r][next_c]:
+                    next_f = f + 1 + H[next_r][next_c] - H[r][c]
+                else:
+                    next_f = f + 1
+
+                if next_f < fuels[next_r][next_c]:
+                    fuels[next_r][next_c] = next_f
+                    heapq.heappush(stack, (next_f, next_r, next_c))
+
+    print(f'#{tc} {fuels[N-1][N-1]}')
